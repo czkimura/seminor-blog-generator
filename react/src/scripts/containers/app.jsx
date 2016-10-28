a88214141ebf6ab2c9db7f544db8a1c4acd7760c
yukihiro-kimura <kimura-yukihiro@cyber-z.co.jp>
2016-10-28 12:16:12 +0900

    ちょっと書き直してみた

--- react/src/scripts/containers/app.jsx
+++ react/src/scripts/containers/app.jsx
@@ -3,18 +3,27 @@ import { connect } from 'react-redux'
 import App from '../components/app'
 import AppActions from '../actions/app'
 
-function mapStateToProps(state) {
+const mapStateToProps = (state) => {
   return state
 }
 
-function mapDispatchToProps(dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return {
     handleClick: () => { dispatch(AppActions.increment()) }
   }
 }
 
 //connectでReduxとReactコンポーネントを繋ぐ
-export default connect(
+@connect(
   mapStateToProps,
   mapDispatchToProps
-)(App)
+)
+class AppContainer extends React.Component {
+  render() {
+    const { fuga, handleClick } = this.props;
+    return (
+      <App fuga={fuga} handleClick={handleClick} />
+    );
+  }
+}
+export default AppContainer;
